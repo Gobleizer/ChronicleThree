0aa5d504fe2889f53a252d6e3b6341c24595c716
Kevin Goble <goble.kevin@gmail.com>
2023-12-11 23:53:54 -0800

    Improves new game state

--- memorymatch/App.js
+++ memorymatch/App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { useReducer } from 'react';
 
+const SYMBOLS = ['a', 'b', 'c'];
+
 function reducer(state, action) {
   switch (action.type) {
     case 'make-a-guess':
@@ -9,27 +11,46 @@ function reducer(state, action) {
     case 'reset-guess':
       return state;
     case 'reset-game':
-      return state;
+      return getNewState();
   } 
   return state;
 }
 
-function getInitialState() {
+function getNewState() {
+  const board = [...SYMBOLS, ...SYMBOLS];
+  shuffle(board);
   return { 
-          board: ['a', 'b', 'a', 'b'],
+          board,
           revealed: [],
           numberOfGuesses: 0
         };
 }
 
+//https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const randomIndex = getRandomIndex(i);
+    swap(arr, randomIndex, i);
+  }
+} 
+
+function getRandomIndex(len) {
+  return Math.floor(Math.random() * len);
+}
+
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, undefined, getInitialState);
+  const [state, dispatch] = useReducer(reducer, undefined, getNewState);
 
 
   return (
     <View style={styles.container}>
       <Button title={"Make A Guess"} onPress={() => dispatch({type: 'make-a-guess'})}></Button>
       <Text>{JSON.stringify(state, null, 2)}</Text>
+      <Button title={"Reset Game"} onPress={() => dispatch({type: 'reset-game'})}></Button>
       <StatusBar style="auto" />
     </View>
   );
